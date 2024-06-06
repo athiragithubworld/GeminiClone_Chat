2e5a2820727faf1bb69fab2d7957a6b7213c4fce
athiragithubworld <athirahrms@gmail.com>
2024-06-06 11:40:37 +0530

    App files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './App.css'
+import Sidebar from './Components/Sidebar/Sidebar'
+import MainPage from "./Components/main/MainPage";
+
+function App() {
+  
+
+  return (
+    <>
+      <Sidebar />
+      <MainPage/>
+    </>
+  )
+}
+
+export default App
